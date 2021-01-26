81dfd0da765eae407a1487b5f33b834864743773
Rab Horner <rab_horner@outlook.com>
2021-01-26 20:26:40 +0000

    Setting of checked state and delete buttons added

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -12,16 +12,17 @@ export class TodoItem extends Component {
             }
         }
 
-    markComplete = (e) => {
-        console.log(this.props);
-    }
-    
+
     render() {
+        const {id, title} = this.props.todo;
+
         return (
             <div style={this.getStyle()}>
                 <p>
-                    <input type="checkbox" onChange={this.markComplete}/> {' '}
-                    {this.props.todo.title}</p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
+                    {title}
+                    <button style={btnStyle} >X</button>
+                </p>  
             </div>
         )
     }
@@ -29,6 +30,16 @@ export class TodoItem extends Component {
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
 }
 
 export default TodoItem
